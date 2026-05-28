fd16d55bfaacb790714ab65713e353311689b4f7
Hugo <malezethp@gmail.com>
2026-05-28 22:09:58 +0200

    feat(setup): recycling old code + setuping css variables

---
+++ src/lib/components/Checkbox.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+    let {
+        svg = "dragons_logos/normal/Artrish.svg",
+        checked = false,
+        id = "",
+        type = "radio",
+        name = "",
+    } = $props();
+</script>
+
+<label class="group relative flex w-[100px] h-[100px] max-[600px]:w-[67px] max-[600px]:h-[67px] box-border p-0.5 cursor-pointer border border-[var(--color-accent)] rounded-[var(--radius)]">
+    <input {type} {id} {checked} {name} class="peer hidden" />
+    <img 
+        src={svg} 
+        alt="" 
+        class="w-full h-full object-cover transition-all duration-200 ease-in-out [filter:invert(90%)_sepia(13%)_saturate(311%)_hue-rotate(52deg)_brightness(101%)_contrast(81%)] peer-checked:[filter:invert(78%)_sepia(52%)_saturate(214%)_hue-rotate(293deg)_brightness(95%)_contrast(88%)] group-hover:[filter:invert(78%)_sepia(52%)_saturate(214%)_hue-rotate(293deg)_brightness(95%)_contrast(88%)]" 
+    />
+</label>
---
+++ src/lib/components/Cta.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    let { dragon = 'Artrish', border = 'Yinva', text, onClick = defaultClick } = $props();
+
+    function defaultClick() {
+    }
+</script>
+
+<button 
+    style="background-color: var(--color-{dragon}); border-color: var(--color-{border});" 
+    onclick={onClick}
+    class="font-text relative flex h-12 w-full cursor-pointer items-center justify-center rounded-[var(--radius)] border text-[20px] text-center transition-all duration-200 ease-in-out hover:opacity-80"
+>
+    <img src="/dragons_logos/normal/{dragon}.svg" alt="" class="absolute right-[20%] top-0 h-full w-auto opacity-30" />
+    {text}
+</button>
